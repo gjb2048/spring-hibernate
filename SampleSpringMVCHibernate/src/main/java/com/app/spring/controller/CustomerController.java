b4c887c8679ab51b2e3d54a4e211582f60259b1a
Gareth J Barnard <gjbarnard@gmail.com>
2014-08-02 13:39:07 +0100

    Update servlet-context.xml.

--- SampleSpringMVCHibernate/src/main/java/com/app/spring/controller/CustomerController.java
+++ SampleSpringMVCHibernate/src/main/java/com/app/spring/controller/CustomerController.java
@@ -18,7 +18,7 @@ public class CustomerController {
 
 	@Autowired(required = true)
 	@Qualifier(value = "customerService")
-	public void setPersonService(CustomerInterface cs) {
+	public void setCustomerService(CustomerInterface cs) {
 		this.customerService = cs;
 	}
 
